4fdcc19d87c8822f20f54221b1d607f1360a1fa4
HyojuKim <mok03189@naver.com>
2023-09-18 17:53:38 +0900

    1

--- Python/23/09/18/sw_13124. 이진 탐색.py
+++ Python/23/09/18/sw_13124. 이진 탐색.py
@@ -1,4 +1,37 @@
+def binarysearch(lst, key):
+    global cnt
+
+    low = 0
+    high = N-1
+    check = 0
+    while low <= high:
+        mid = (low+high) // 2
+        if A[mid] == key:
+            cnt += 1
+            return
+        elif A[mid] > key:
+            if check == 1:
+                break
+            else:
+                high = mid - 1
+                check = 1
+        else:
+            if check == 2:
+                break
+            else:
+                low = mid + 1
+                check = 2
+    return
+
+
 T = int(input())
 for tc in range(1, T+1):
     N, M = map(int, input().split())
-    
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+    A.sort()
+    cnt = 0
+    for i in range(len(B)):
+        key = B[i]
+        binarysearch(A, key)
+    print(f'#{tc} {cnt}')
