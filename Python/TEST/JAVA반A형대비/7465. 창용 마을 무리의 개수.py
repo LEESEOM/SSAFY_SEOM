79cc863ccc0e0a2c01962a40f0e5817a300e9367
seom <dltjdah25@naver.com>
2023-10-11 08:18:09 +0900

    1

---
+++ Python/TEST/JAVA반A형대비/7465. 창용 마을 무리의 개수.py
@@ -0,0 +1,24 @@
+from collections import deque
+
+T = int(input())
+for tc in range(1, T+1):
+    N, M = map(int, input().split())
+    arr = [[0]*N for _ in range(N)]
+    visited = [0]*N
+    cnt = 0
+    for i in range(M):
+        x, y = map(int, input())
+        arr[x][y] = arr[y][x] = 1
+    deq = deque()
+    for j in range(N):
+        if visited[j] == 0:
+            deq.append(j)
+            visited[j] = 1
+            cnt += 1
+            while deq:
+                now = deq.popleft()
+                for k in range(N):
+                    if arr[now][k] == 1 and visited[k] == 0:
+                        deq.append(k)
+                        visited[k] =1
+    print(f'#{tc} {cnt}')
