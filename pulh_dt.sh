7b0f8472215b47cce5de73fdf66d7a12b4a4d964
hyunjihye <hhh259@naver.com>
2023-10-11 09:53:32 +0900

    1

---
+++ pulh_dt.sh
@@ -0,0 +1,14 @@
+C:\Users\USER\Desktop\SSAFY_SEOM
+
+#!/bin/bash
+# 이 스크립트는 현재 디렉터리에서 Git 저장소로 가정합니다.
+
+# Git 저장소 경로
+git_repo_path="C:\Users\USER\Desktop\SSAFY_SEOM"
+
+# Git 명령 실행
+cd "$git_repo_path"
+git pull origin main
+git add .
+git commit -m '1'
+git push origin main
