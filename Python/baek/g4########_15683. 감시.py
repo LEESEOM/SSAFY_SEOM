74051a9c18ce9b3f46b76cd9ed10557d23df0e6a
seom <dltjdah25@naver.com>
2023-10-23 20:40:00 +0900

    1

--- Python/baek/g4########_15683. 감시.py
+++ Python/baek/g4########_15683. 감시.py
@@ -3,10 +3,18 @@ from collections import deque
 N, M = map(int, input().split())
 info = [list(map(int, input().split())) for _ in range(N)]
 ans = float('inf')
-
+# 상하좌우
+dr = [1,-1,0,0]
+dc = [0,0,-1,1]
 # 1: 우, 2: 반대, 3: 직각, 4: 삼방, 5: 사방
-# 1,3,4 = 4번 다돌리기 / 2 = 1번만 돌리기 / 5 = 안돌려도됨
-
+turn = [
+    [],
+    [[0],[1],[2],[3]],
+    [[0,1],[2,3]],
+    [[0,2],[0,3],[1,2],[1,3]],
+    [[0,1,2],[0,1,3],[1,2,3],[0,2,3]],
+    [[0,1,2,3]]
+]
 # cctv 돌리는 조합인데 순서라기보단 1-1~4, 2-1or2 이런 느낌이라 어떻게 할 지 모르겠네
 
         # 뭔가 순서대로 돌렸다가 손해보면 어쩌으냐
@@ -17,7 +25,7 @@ ans = float('inf')
 
 # 그 조합에서 사각지대 계산
 # 체크할 배열 새로 만들어주고
-method = []
+
 arr = info[:]
 # 방향으로 쭉 바꾸면서 벽만나면 멈추기
 for i in range(len(method)):
@@ -29,7 +37,7 @@ for i in range(len(method)):
             nr = row
             nc = col
             if 0<=nr<N and 0<=nc<M and arr[nr][nc] != '6':
-                arr[nr][nc] = '1'
+                arr[nr][nc] = '9'
                 #deq.append((nr,nc))
     # 다 끝나면 0 갯수 세고 제일 적으면 갱신
     for row in range(N):
