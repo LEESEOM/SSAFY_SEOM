945f469a706640c213e6dbbf1920ad6589c2609c
seom <dltjdah25@naver.com>
2023-10-25 21:17:34 +0900

    1

--- Python/baek/s3#########_9461. 파도반 수열.py
+++ Python/baek/s3#########_9461. 파도반 수열.py
@@ -1,10 +1,11 @@
+dp = [0]*101
+dp[1] = 1
+dp[2] = 1
+dp[3] = 1
+for i in range(4, 101):
+    dp[i] = dp[i-3]+dp[i-2]
+
 T = int(input())
 for _ in range(T):
     N = int(input())
-    dp = [0]*(N+1)
-    dp[1] = 1
-    dp[2] = 2
-    dp[3] = 3
-    for i in range(4, N+1):
-        dp[i] = dp[i-3]+dp[i-2]
     print(dp[N])
