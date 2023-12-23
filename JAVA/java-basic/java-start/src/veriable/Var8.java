b5ca413a5cd1e8cc750150374758539481f795fd
seom <dltjdah25@naver.com>
2023-12-23 20:02:15 +0900

    1

---
+++ JAVA/java-basic/java-start/src/veriable/Var8.java
@@ -0,0 +1,15 @@
+package veriable;
+
+public class Var8 {
+    public static void main(String[] args) {
+        //정수
+        byte b = 126; //128
+        short s = 32767; // 32767
+        int i = 21; // 21억
+        long l = 9L;
+
+        //실수
+        float f = 10.0f;
+        double d = 10.0;
+    }
+}
